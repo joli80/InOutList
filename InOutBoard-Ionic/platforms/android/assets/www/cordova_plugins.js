060668ca56399909b1ba18e54c9379265a19db54
Johan Lindqvist <Johan.Lindqvist@infracontrol.com>
2015-12-12 16:37:22 +0100

    Added "/" to display name

--- InOutBoard-Ionic/platforms/android/assets/www/cordova_plugins.js
+++ InOutBoard-Ionic/platforms/android/assets/www/cordova_plugins.js
@@ -3,38 +3,46 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-ms-adal/www/utility.js",
         "id": "cordova-plugin-ms-adal.utility",
+        "pluginId": "cordova-plugin-ms-adal",
         "runs": true
     },
     {
         "file": "plugins/cordova-plugin-ms-adal/www/AuthenticationContext.js",
         "id": "cordova-plugin-ms-adal.AuthenticationContext",
+        "pluginId": "cordova-plugin-ms-adal",
         "clobbers": [
             "Microsoft.ADAL.AuthenticationContext"
         ]
     },
     {
         "file": "plugins/cordova-plugin-ms-adal/www/CordovaBridge.js",
-        "id": "cordova-plugin-ms-adal.CordovaBridge"
+        "id": "cordova-plugin-ms-adal.CordovaBridge",
+        "pluginId": "cordova-plugin-ms-adal"
     },
     {
         "file": "plugins/cordova-plugin-ms-adal/www/AuthenticationResult.js",
-        "id": "cordova-plugin-ms-adal.AuthenticationResult"
+        "id": "cordova-plugin-ms-adal.AuthenticationResult",
+        "pluginId": "cordova-plugin-ms-adal"
     },
     {
         "file": "plugins/cordova-plugin-ms-adal/www/TokenCache.js",
-        "id": "cordova-plugin-ms-adal.TokenCache"
+        "id": "cordova-plugin-ms-adal.TokenCache",
+        "pluginId": "cordova-plugin-ms-adal"
     },
     {
         "file": "plugins/cordova-plugin-ms-adal/www/TokenCacheItem.js",
-        "id": "cordova-plugin-ms-adal.TokenCacheItem"
+        "id": "cordova-plugin-ms-adal.TokenCacheItem",
+        "pluginId": "cordova-plugin-ms-adal"
     },
     {
         "file": "plugins/cordova-plugin-ms-adal/www/UserInfo.js",
-        "id": "cordova-plugin-ms-adal.UserInfo"
+        "id": "cordova-plugin-ms-adal.UserInfo",
+        "pluginId": "cordova-plugin-ms-adal"
     },
     {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
         "runs": true
     }
 ];
@@ -42,7 +50,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-ms-adal": "0.6.0",
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.2.0"
 }
 // BOTTOM OF METADATA
 });
